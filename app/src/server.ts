ac12e9db8df9cbf71b8b25c11634b6f12e637f17
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-01-19 15:24:14 +0545

    restructure and added typescript

---
+++ app/src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import expressWs from 'express-ws';
+
+const { app } = expressWs(express())
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+});
+
+app.ws('/', function (ws, req) {
+    ws.on('message', function (msg) {
+        console.log(msg);
+    });
+    console.log('socket', req.testing);
+});
+
+app.listen(port, () => {
+    console.log(`Live at http://localhost:${process.env.PORT}`)
+});

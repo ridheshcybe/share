01bb7cbbf6b67ac57a45ca7f033be0b27609814b
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-01-19 16:51:32 +0545

    added index ejs file

--- app/src/server.ts
+++ app/src/server.ts
@@ -8,6 +8,8 @@ const port = process.env.PORT || 8080
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, '../web/views'));
 
+app.use('/css', express.static(path.resolve(__dirname, '../web/css')))
+
 app.get('/', (req, res) => {
     res.render("index.ejs")
 });

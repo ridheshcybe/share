2a8ffd78819a9a778eccd2ba0b0e47614580acc3
ridheshcybe <80441770+ridheshcybe@users.noreply.github.com>
2023-04-12 21:14:29 +0545

    added send.ejs

---
+++ test/index.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const request = require('supertest');
+const index = require("../app/src/index");
+
+describe('router exists', () => {
+    it('should have home (/) page', () => {
+
+    });
+});
